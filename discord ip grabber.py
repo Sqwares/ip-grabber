20d0d20c5e79180550207119b927ff1f7181d0f2
Skurtwares1337 <83078854+Sqwares@users.noreply.github.com>
2021-08-17 11:17:34 +0300

    Add files via upload

---
+++ discord ip grabber.py
@@ -0,0 +1,12 @@
+#coded by Sqwares
+import time
+import os
+print("""
+╔╦╗┬┌─┐┌─┐┌─┐┬─┐┌┬┐  ╦  ╦┌─┐┬┌─┐┌─┐  ┬┌─┐  ┌─┐┬─┐┌─┐┌┐ ┌┐ ┌─┐┬─┐
+ ║║│└─┐│  │ │├┬┘ ││  ╚╗╔╝│ │││  ├┤   │├─┘  │ ┬├┬┘├─┤├┴┐├┴┐├┤ ├┬┘
+═╩╝┴└─┘└─┘└─┘┴└──┴┘   ╚╝ └─┘┴└─┘└─┘  ┴┴    └─┘┴└─┴ ┴└─┘└─┘└─┘┴└─
+""")
+        
+hedef = input("Target ex russia338 : ")
+os.system("ping " + hedef+".discord.gg ")
+time.sleep(0.8)
